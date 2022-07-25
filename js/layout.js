f001121deadde31ab79747f26a75a4c92532dcc2
pparksso <thgml5143@hanmail.net>
2022-07-25 21:48:49 +0900

    반응형 거의 끝나감

--- js/layout.js
+++ js/layout.js
@@ -3,6 +3,9 @@ const infoBtn = document.querySelector("#footer .richwood");
 const info = document.querySelector("#footer .info");
 const subDepth01 = document.querySelectorAll("#subHeader .depth01");
 const subDepth02 = document.querySelectorAll("#subHeader .depth02");
+const subHeader = document.querySelector("#subHeader");
+const hambergerBtn01 = document.querySelector("#gnbBtns .hamberger");
+const hanmbergerBtn02 = document.querySelector("#subHeader .hamberger");
 
 window.addEventListener("wheel", (e) => {
   let scrollTop = document.documentElement.scrollTop;
@@ -21,10 +24,18 @@ subDepth01.forEach((item, index) => {
     e.preventDefault();
     subDepth02.forEach((item2, index02) => {
       if (index === index02) {
-        item2.classList.add("open");
+        item2.classList.toggle("open");
       } else {
         item2.classList.remove("open");
       }
     });
   });
+});
+
+hambergerBtn01.addEventListener("click", () => {
+  subHeader.classList.add("open");
+});
+
+hanmbergerBtn02.addEventListener("click", () => {
+  subHeader.classList.remove("open");
 });
